f47e248155bdff4607eb5ec932048443d679e68d
Shimon Shvartsbroit <shimon1981@gmail.com>
2017-01-20 19:50:19 +0200

    init

---
+++ handycast/handycast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  handycast
+//
+//  Created by Shimon Shvartsbroit on 20/01/2017.
+//  Copyright © 2017 Shimon Shvartsbroit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
